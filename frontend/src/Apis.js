942d1fdc85906e9f62cc6be6fd76c08d317f5f17
hikmatrah400 <hikmat.jan.rahmati@gmail.com>
2024-12-07 14:00:39 -0800

    Update Apis.js

--- frontend/src/Apis.js
+++ frontend/src/Apis.js
@@ -1,20 +1,4 @@
-export const emptyApi = "http://localhost:42734/empty";
-export const purchaseListApi = "http://localhost:42734/db-purchase-of-the-Data";
-export const stockPruchaseApi =
-  "http://localhost:42734/db-stock-purchase-of-the-Data";
-
-export const sellListApi = "http://localhost:42734/db-sell-of-the-Data";
-export const stockSellApi = "http://localhost:42734/db-stock-sell-of-the-Data";
-
-export const expensesApi =
-  "http://localhost:42734/db-afghan-land-expenses-of-the-Data";
-export const stockExpenseApi =
-  "http://localhost:42734/db-stock-expenses-of-the-Data";
-
-export const historysApi =
-  "http://localhost:42734/db-part-of-history-of-the-Data";
-
-const path = "http://localhost:10000";
+const path = "https://database-backend-jnnj.onrender.com";
 
 export const purchaseApi = `${path}/purchase`;
 export const sellApi = `${path}/sell`;
